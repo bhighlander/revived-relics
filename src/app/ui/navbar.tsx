db21dd79d4f0cf853c094cb5ac82646b3a122315
bhighlander <saphrimangel@gmail.com>
2024-03-22 13:12:21 -0500

    added desktop navbar and installed mui drawer

---
+++ src/app/ui/navbar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <div className="flex items-center justify-between bg-gray-800 text-white p-4">
+            <div className="flex items-center space-x-4">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+            </div>
+            <div className="flex items-center">
+                <Image 
+                    src="/public/logotransparent.png"
+                    width={50}
+                    height={50}
+                    alt="Revived Relics" 
+                />
+            </div>
+            <div className="flex items-center space-x-4">
+                <Link href="/projects">Projects</Link>
+                <Link href="/contact">Contact</Link>
+            </div>
+        </div>
+    );
+}
