c141eaa1ee19cb93eb3a61e05a8d0ebe0f01fdeb
Zach D'Amico <zachdamico90@gmail.com>
2024-03-27 16:59:50 -0500

    footer start more styling needed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./ui/navbar";
+import Footer from "./ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <head></head>
       <body className={inter.className}>
-      <Navbar />
-      {children}
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
