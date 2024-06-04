1cb157d1ad29ec50f61d0dfb12afb06f7c914ed2
bhighlander <saphrimangel@gmail.com>
2024-06-04 16:39:50 -0500

    refactored home page styling

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,9 +3,11 @@ import HomeCarousel from "../homeUi/homeImageCarousel";
 
 export default function HomePage() {
   return (
-    <>
-    <HomeCarousel />
-    <ContactComponent />
-    </>
-  )
+    <div className="flex flex-col items-center justify-top min-h-screen space-y-8 pt-8 w-full">
+      <h1 className="rr-header text-5xl font-bold text-center">Revived Relics</h1>
+      <p className="rr-tag text-2xl text-center">Restoring life to the things you love</p>
+      <HomeCarousel />
+      <ContactComponent />
+    </div>
+  );
 }
